f70e6cdc45948bc4eddf136ed4e92a9c98908034
Sahar Pich <saharp6@gmail.com>
2026-03-14 18:44:11 +0200

    Fix routes and add basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/Cryptonite/">
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
